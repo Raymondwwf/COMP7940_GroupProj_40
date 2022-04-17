1f9eff6845ac786fd4eca206b01b5cc66eae1146
tempowong <tempowong28@gmail.com>
2022-04-17 11:02:04 +0800

    update docker file

--- Docker/comp7940_GP40_sql/createuser.sql
+++
@@ -1,8 +0,0 @@
-use mysql;
-select host, user from user;
-
-create user docker identified by '123456';
-
-grant all on comp7940_group40.* to docker@'%' identified by '123456' with grant option;
-
-flush privileges;
