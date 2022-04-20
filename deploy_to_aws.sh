3df243cce627ebe17b4a41d3228281d899ff3ae8
tempowong <62502152+tempowong@users.noreply.github.com>
2022-04-20 23:56:21 +0800

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 echo 'Starting to Deploy...'
-ssh mailto:ec2-user@ec2-16-162-122-188.ap-east-1.compute.amazonaws.com " sudo docker image prune -f 
+ssh ec2-user@ec2-18-163-195-22.ap-east-1.compute.amazonaws.com " sudo docker image prune -f 
         cd COMP_GIT 
         sudo docker-compose down
         git fetch origin
